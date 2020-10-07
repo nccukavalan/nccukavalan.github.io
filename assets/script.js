2c09c6a0de22bc480ef8389e3cf04c650f0619dc
nccukavalan <62790771+nccukavalan@users.noreply.github.com>
2020-10-07 22:55:44 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -187,7 +187,7 @@ function main() {
                 btn.type = 'button';
                 btn.name = moment(dateValue).format('YYYY-MM-DD');
                 btn.value = moment(dateValue).format('YYYY-MM-DD') + ' ' + available[i].period[j];
-                btn.innerHTML = moment(dateValue).format('M[月]D[日]') + '（' + week[dateValue.getUTCDay()] + '）' + available[i].period[j];
+                btn.innerHTML = moment(dateValue).format('M[月]D[日]') + '（' + week[dateValue.getDay()] + '）' + available[i].period[j];
                 radioRow.appendChild(btn);
                 radio.appendChild(radioRow);
             }
